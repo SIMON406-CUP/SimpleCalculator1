30e2189e435c85312d6246757761ef4a0a5f15ac
SIMON406-CUP <simonmothoka40@gmail.com>
2025-11-06 18:53:21 +0200

    Added a select operation to choose from addition, subtraction, multiplication and division when calculating

--- SimpleCalculator/SimpleCalculator.py
+++ SimpleCalculator/SimpleCalculator.py
@@ -1,9 +1,12 @@
-# ask the user to enter an operation
-print("Select operation")
-print("1. Add")
+print("welcome to calculator")
+
+# Ask the user to enter an operation)
+print("\n select operation")
+print("1: Add")
 print("2. Subtract")
 print("3. Multiply")
 print("4. Divide")
+
 
 # get input from the user
 choice = input("Enter choice (1/2/3/4): ")
